8714f73d69674382ad522ece6180049bab230ef6
Emil lindgren <hoganas_1937_70@hotmail.com>
2016-06-02 13:24:24 +0200

    dezor 1

---
+++ HtmlLabs/Laboration4/scripts/snap.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $('#butsnap').on('click', snap);
+});
+
+function snap() {
+    var canvas = document.getElementById('myCanvas');
+    var videon = document.getElementById('mvideo');
+    canvas.getContext('2d').drawImage(videon, 0, 0, 200, 150);
+}
